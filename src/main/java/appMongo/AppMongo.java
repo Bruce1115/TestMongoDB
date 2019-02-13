6d48134c7dbae0f354366df0b5ec4280318317ff
勇 李 <Bruce1115@qq.com>
2019-02-13 13:11:50 +0800

    first

---
+++ src/main/java/appMongo/AppMongo.java
@@ -0,0 +1,54 @@
+package appMongo;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import mongo.entity.UserInfo;
+import mongo.service.UserService;
+
+public class AppMongo {
+
+	public static void main(String[] args) {
+		new AppMongo().TestQuery();
+	}
+
+	private void TestQuery() {
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("springmgo.xml");
+		UserService service = (UserService) ctx.getBean(UserService.class);
+		UserInfo user = new UserInfo();
+		user.setName("王五");
+		user.setAge(29);
+		user.setBirth(new Timestamp(System.currentTimeMillis()));
+		List<UserInfo> list = null;
+		try {
+			list = service.findAll();
+		} catch (Exception e) {
+			System.out.print(e.getLocalizedMessage());
+		}
+		System.out.println("查询到:" + list.size());
+
+		ctx.close();
+	}
+
+	private static void TestSave() {
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("springmgo.xml");
+		UserService service = (UserService) ctx.getBean(UserService.class);
+		UserInfo user = new UserInfo();
+		user.setName("王五");
+		user.setAge(29);
+		user.setBirth(new Timestamp(System.currentTimeMillis()));
+
+		try {
+			service.save(user);
+		} catch (Exception e) {
+			System.out.print(e.getLocalizedMessage());
+		}
+		System.out.println("已生成ID:" + user.getId());
+
+		ctx.close();
+	}
+
+}
---
+++ src/main/java/mongo/service/UserService.java
@@ -0,0 +1,39 @@
+package mongo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mongo.dao.IBaseDao;
+import mongo.dao.IUserDao;
+import mongo.entity.UserInfo;
+
+/**
+ * 用户service类
+ * <p>
+ * ClassName: UserService
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: (c)2017 Jastar·Wang,All rights reserved.
+ * </p>
+ * 
+ * @author Jastar·Wang
+ * @date 2017年4月12日
+ */
+@Service
+public class UserService extends BaseService<UserInfo> {
+
+	@Autowired
+	private IUserDao userDao;
+
+	@Override
+	protected IBaseDao<UserInfo> getDao() {
+		return userDao;
+	}
+
+	public void testHello() {
+		System.out.println("----hello");
+	}
+}
